f56dc624cc918baf701109acbb835081c6cdd343
unknown <shikhagupta698@gmail.com>
2025-06-22 17:30:40 +0530

    Initial commit

---
+++ src/ATB/Lab01.java
@@ -0,0 +1,13 @@
+package ATB;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Lab01 {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+        System.out.println();
+    }
+
+}
